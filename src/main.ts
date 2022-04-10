2a086f2da4dfcfb9d61911b27c9dd8a8ab28226f
logosrhema01 <achiampongk22@gmail.com>
2022-04-10 22:10:59 +0000

    feat: aws deploy pipleline

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages:
+        process.env.NODE_ENV === 'production' ? true : false,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true, // allow conversion underneath
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Alte example')
+    .setDescription('The Alte API description')
+    .setVersion('1.0')
+    .addTag('default')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
