ecce144be2b88b19d93b380e820ff62fda8e8d04
logosrhema01 <achiampongk22@gmail.com>
2022-04-12 20:08:54 +0000

    feat: updated driver api schema

--- src/driver/driver.controller.ts
+++ src/driver/driver.controller.ts
@@ -10,29 +10,73 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { DriverService } from './driver.service';
 import { CreateDriverDto } from './dto/create-driver.dto';
 import { UpdateDriverDto } from './dto/update-driver.dto';
+import { Driver } from './entities/driver.entity';
 
 @ApiTags('drivers')
 @Controller('drivers')
 export class DriverController {
   constructor(private readonly driverService: DriverService) {}
 
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        email: {
+          type: 'string',
+          format: 'email',
+        },
+        password: {
+          type: 'string',
+        },
+      },
+      required: ['email', 'password'],
+    },
+  })
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
-  async login(@Request() req) {
+  @ApiResponse({
+    status: 200,
+    description: 'Accepted',
+    schema: {
+      type: 'object',
+      properties: {
+        access_token: {
+          type: 'string',
+        },
+      },
+      required: ['access_token'],
+    },
+  })
+  async login(@Request() req): Promise<{ access_token: string }> {
     return this.driverService.login(req.user);
   }
 
   @UseGuards(AuthGuard('jwt:driver'))
+  @ApiResponse({ type: Driver })
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
   }
 
   @Post()
+  @ApiResponse({
+    status: 200,
+    description: 'Accepted',
+    schema: {
+      type: 'object',
+      properties: {
+        access_token: {
+          type: 'string',
+        },
+      },
+      required: ['access_token'],
+    },
+  })
+  @ApiBody({ type: CreateDriverDto })
   create(@Body() createDriverDto: CreateDriverDto & { salt: string }) {
     return this.driverService.create(createDriverDto);
   }
@@ -42,6 +86,7 @@ export class DriverController {
     return this.driverService.findAll();
   }
 
+  @ApiResponse({ type: Driver })
   @Get(':email')
   findOne(@Param('email') email: string) {
     return this.driverService.findOne(email);
